efbbad4a83e9bc0900f707895469a249728fb68c
kozakevych <andriikozakevych@gmail.com>
2017-04-11 14:40:48 +0300

    created test db and tried

---
+++ src/getvalues.js
@@ -0,0 +1,31 @@
+
+/*
+**
+
+all info about knives (pictures, names, prices) was taken from here
+-->
+
+https://www.knifecenter.com/kc_new/store_store.html?brand=234&min_price=100&max_price=500&country=&design=&steel=&c=&locktype=&style=&min_blade=0&max_blade=50
+
+with usage of simple dom-js
+
+and saved into knifedb.json
+
+**
+*/
+
+var knives = [];
+
+var l = knife.title = document.getElementsByClassName("product_name").length; //28
+
+for (i=0; i<l; i++) {
+	var knife = {};
+
+	knife.title = document.getElementsByClassName("product_name")[i].title;
+	knife.img = $( "img[alt*='Spyderco']" )[i].currentSrc;
+	knife.price = document.getElementsByClassName("our_price")[i].innerText;
+
+	knives.push(knife);
+}
+
+JSON.stringify(knives);
